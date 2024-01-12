272f7ff1023352edcc9a6ff74a4739baa564a8d0
CSturzaker <craig.sturzaker@arup.com>
2024-01-12 21:00:42 +0000

    feat(http requests): setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterOutlet } from "@angular/router";
+
+import { HttpServiceService } from "./http-service.service";
 
 @Component({
-  selector: 'app-root',
+  selector: "app-root",
   standalone: true,
   imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'cors-testing';
+  private httpService = inject(HttpServiceService);
+
+  title = "cors-testing";
+
+  constructor() {
+    this.httpService.getClasses().subscribe((res) => {
+      console.log("Classes: ", res);
+    });
+
+    this.httpService.flagToInvestigate().subscribe((res) => {
+      console.log("Flag: ", res);
+    });
+  }
 }
---
+++ src/app/http-service.service.ts
@@ -0,0 +1,63 @@
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from "@angular/common/http";
+import { Injectable, inject } from "@angular/core";
+import { catchError, throwError } from "rxjs";
+
+@Injectable({
+  providedIn: "root",
+})
+export class HttpServiceService {
+  private http = inject(HttpClient);
+
+  getClasses() {
+    const httpOptions = {
+      withCredentials: true,
+    };
+    return this.http
+      .get(
+        "",
+        httpOptions
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  flagToInvestigate() {
+    const httpHeaders = new HttpHeaders({
+      "Content-Type": "application/json; text/plain",
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Origin": "*",
+    });
+
+    return this.http
+      .post(
+        "",
+        { uri: "http://nationaldigitaltwin.gov.uk/data#building_10094231272" },
+        {
+          headers: httpHeaders,
+          withCredentials: false,
+        }
+      )
+      .pipe(catchError(this.handleError));
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error("An error occurred:", error.error);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong.
+      console.error(
+        `Backend returned code ${error.status}, body was: `,
+        error.error
+      );
+    }
+    // Return an observable with a user-facing error message.
+    return throwError(
+      () => new Error("Something bad happened; please try again later.")
+    );
+  }
+}
